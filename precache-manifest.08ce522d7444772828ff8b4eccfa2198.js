6b007afcfa1f7053360304a338d8ad1ca3636fd5
David Napadensky <davidnapadensky@gmail.com>
2020-05-28 14:39:37 -0300

    Updates

---
+++ precache-manifest.08ce522d7444772828ff8b4eccfa2198.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "235304cec42260bbd8b188a14771675b",
+    "url": "/React-2/index.html"
+  },
+  {
+    "revision": "1c50e388228bc3dd010a",
+    "url": "/React-2/static/css/main.b486a8d4.chunk.css"
+  },
+  {
+    "revision": "803ab6d66de6cb164f26",
+    "url": "/React-2/static/js/2.42b9e8cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-2/static/js/2.42b9e8cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c50e388228bc3dd010a",
+    "url": "/React-2/static/js/main.7f816f20.chunk.js"
+  },
+  {
+    "revision": "02635c94e649f6d7b900",
+    "url": "/React-2/static/js/runtime-main.bf9f51bb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-2/static/media/logo.5d5d9eef.svg"
+  }
+]);
